d91b36739167f4f1f226c5547fdd77d2734520d8
ucyy34 <ucyarts@gmail.com>
2025-09-01 03:01:01 +0300

    Update medusa-config.ts

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -19,71 +19,16 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || 'supersecret'
     }
   },
+
   modules: [
-    { resolve: '@mercurjs/seller' },
-    { resolve: '@mercurjs/reviews' },
-    { resolve: '@mercurjs/marketplace' },
-    { resolve: '@mercurjs/configuration' },
-    { resolve: '@mercurjs/order-return-request' },
-    { resolve: '@mercurjs/requests' },
-    { resolve: '@mercurjs/brand' },
-    { resolve: '@mercurjs/wishlist' },
-    { resolve: '@mercurjs/split-order-payment' },
-    { resolve: '@mercurjs/attribute' },
-    {
-      resolve: '@mercurjs/taxcode',
-      options: {
-        apiKey: process.env.STRIPE_SECRET_API_KEY
-      }
-    },
-    { resolve: '@mercurjs/commission' },
-    {
-      resolve: '@mercurjs/payout',
-      options: {
-        apiKey: process.env.STRIPE_SECRET_API_KEY,
-        webhookSecret: process.env.STRIPE_CONNECTED_ACCOUNTS_WEBHOOK_SECRET
-      }
-    },
-    {
-      resolve: '@mercurjs/algolia',
-      options: {
-        apiKey: process.env.ALGOLIA_API_KEY,
-        appId: process.env.ALGOLIA_APP_ID
-      }
-    },
-    {
-      resolve: '@medusajs/medusa/payment',
-      options: {
-        providers: [
-          {
-            resolve: '@mercurjs/payment-stripe-connect',
-            id: 'stripe-connect',
-            options: {
-              apiKey: process.env.STRIPE_SECRET_API_KEY
-            }
-          }
-        ]
-      }
-    },
     {
       resolve: '@medusajs/medusa/notification',
       options: {
         providers: [
           {
-            resolve: '@mercurjs/resend',
-            id: 'resend',
-            options: {
-              channels: ['email'],
-              api_key: process.env.RESEND_API_KEY,
-              from: process.env.RESEND_FROM_EMAIL
-            }
-          },
-          {
             resolve: '@medusajs/medusa/notification-local',
             id: 'local',
-            options: {
-              channels: ['feed', 'seller_feed']
-            }
+            options: { channels: ['feed'] }
           }
         ]
       }
